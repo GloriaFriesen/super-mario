aa2fb3f46c8f982160ee317edd61f7589e0e0d48
Megan Warnock <megandwarnock@gmail.com>
2017-06-07 11:31:43 -0700

    set up initial file structure

---
+++ desktop/src/com/megandwarnock/supermario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.megandwarnock.supermario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.megandwarnock.supermario.SuperMario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperMario(), config);
+	}
+}
