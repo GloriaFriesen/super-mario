0f0840ab99c8c1d7b92d55e56aa54c54e192732b
Gloria Friesen and Megan Warnock <gloria@>
2017-06-07 13:35:33 -0700

    add OrthographicCamera and viewport

--- core/src/com/megandwarnock/supermario/Screens/PlayScreen.java
+++ core/src/com/megandwarnock/supermario/Screens/PlayScreen.java
@@ -3,7 +3,12 @@ package com.megandwarnock.supermario.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.megandwarnock.supermario.SuperMario;
 
 /**
@@ -12,12 +17,15 @@ import com.megandwarnock.supermario.SuperMario;
 
 public class PlayScreen implements Screen {
     private SuperMario game;
-
+    private OrthographicCamera gamecam;
     Texture texture;
+    private Viewport gamePort;
 
     public PlayScreen(SuperMario game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gamecam);
     }
 
     @Override
@@ -29,6 +37,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -36,7 +45,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
